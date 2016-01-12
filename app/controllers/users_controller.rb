77d3b2ddf45a102a99eff2fa47375925252482f3
sutatin <sutatinsutatin+cloud9@gmail.com>
2016-01-12 08:54:00 +0000

    add colum profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name,:email,:region,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:region,:profile,:password,:password_confirmation)
   end
   
   def set_user
